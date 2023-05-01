49ff6527a83556edd596b880dc0120399f28b481
mills <haifeng.zhou@live.com>
2023-05-01 21:52:34 +0800

    added http server for healthcheck

--- master/master.py
+++ master/master.py
@@ -1,8 +1,21 @@
 
 package master
-
+from http.server import SimpleHTTPRequestHandler
+from http.server import CGIHTTPRequestHandler
+from http.server import ThreadingHTTPServer
+from functools import partial
 from enum import Enum
+import contextlib
+import sys
+import os
 import etcd
+
+class DualStackServer(ThreadingHTTPServer):
+    def server_bind(self):
+        # suppress exception when protocol is IPv4
+        with contextlib.suppress(Exception):
+            self.socket.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, 0)
+        return super().server_bind()
 
 class MasterState(enum):
     Leader = 1
@@ -13,6 +26,41 @@ class Master(object):
         self.parent = None 
         self.master_state_watcher = None
         self.metadata = Tree(1000)
+    #start http server for healthcheck
+    def start_httpserver(server_class=DualStackServer,
+            handler_class=SimpleHTTPRequestHandler,
+            port=8000,
+            bind='127.0.0.1',
+            cgi=False,
+            directory=os.getcwd()):
+        """Run an HTTP server on port 8000 (or the port argument).
+
+        Args:
+            server_class (_type_, optional): Class of server. Defaults to DualStackServer.
+            handler_class (_type_, optional): Class of handler. Defaults to SimpleHTTPRequestHandler.
+            port (int, optional): Specify alternate port. Defaults to 8000.
+            bind (str, optional): Specify alternate bind address. Defaults to '127.0.0.1'.
+            cgi (bool, optional): Run as CGI Server. Defaults to False.
+            directory (_type_, optional): Specify alternative directory. Defaults to os.getcwd().
+        """
+
+        if cgi:
+            handler_class = partial(CGIHTTPRequestHandler, directory=directory)
+        else:
+            handler_class = partial(SimpleHTTPRequestHandler, directory=directory)
+
+        with server_class((bind, port), handler_class) as httpd:
+            print(
+                f"Serving HTTP on {bind} port {port} "
+                f"(http://{bind}:{port}/) ..."
+            )
+            try:
+                httpd.serve_forever()
+            except KeyboardInterrupt:
+                print("\nKeyboard interrupt received, exiting.")
+                sys.exit(0)
+
+
     #借助与etcd进行选主,
     def elect_master(self):  
         self.master_state_watcher = EtcdWatcher()
@@ -29,7 +77,11 @@ if __name__ == "__main__":
     try:
         #master选举, 在需要使用master/slave状态的地方
         # 通过master_state_watcher进行获取.
-        eletct_master()
+        master = Master()
+        # 启动http server for healthcheck
+        master.start_httpserver()
+        # master选主
+        master.eletct_master()
 
     except Exception:
         logger.error("Etcd watcher error: {}".format(traceback.format_exc()))
